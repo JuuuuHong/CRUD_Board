0a4be7ead7ff2a24bb07d1a6e522603392b77470
JuuuuHong <wnghd720@naver.com>
2022-11-22 22:47:33 +0900

    페이징 처리

--- src/main/java/com/study/board/contoller/BoardController.java
+++ src/main/java/com/study/board/contoller/BoardController.java
@@ -3,6 +3,10 @@ package com.study.board.contoller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 
 @Controller
 public class BoardController {
@@ -33,9 +36,21 @@ public class BoardController {
     }
 
     @GetMapping("/board/list")
-    public String boardList(Model model){
+    public String boardList(Model model, @PageableDefault(page = 0, size = 10, sort="id",
+            direction = Sort.Direction.DESC) Pageable pageable){
 
-        model.addAttribute("list", boardService.boardList());
+        Page<Board> list = boardService.boardList(pageable);
+
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        model.addAttribute("list", boardService.boardList(pageable));
 
         return "boardlist";
     }
